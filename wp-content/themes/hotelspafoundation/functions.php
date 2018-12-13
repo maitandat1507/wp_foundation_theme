78d35f447f6ef0a16e72b300807a8916db10741c
Mai Tấn Đạt <maitandat1507@gmail.com>
2018-12-13 09:55:17 +0700

    apply scripts & create new Home page

--- wp-content/themes/hotelspafoundation/functions.php
+++ wp-content/themes/hotelspafoundation/functions.php
@@ -4,7 +4,9 @@ function hotelspa_styles() {
     wp_enqueue_style('app', get_template_directory_uri() . '/css/app.css', '1.0.0');
     wp_enqueue_style('foundation-icons', get_template_directory_uri() . '/css/foundation-icons.css', '1.0.0');
 
-    wp_enqueue_script('jquery', get_template_directory_uri() . '/js/app.js');
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('what-input', get_template_directory_uri() . '/bower_components/what-input/what-input.js', array('jquery'), '6.2.2', true);
+    wp_enqueue_script('foundation', get_template_directory_uri() . '/bower_components/foundation-sites/dist/foundation.js', array('jquery'), '6.2.2', true);
 }
 
 add_action('wp_enqueue_scripts', 'hotelspa_styles');
--- wp-content/themes/hotelspafoundation/index.php
+++ wp-content/themes/hotelspafoundation/index.php
@@ -298,9 +298,10 @@
 
 
 
-    <script src="bower_components/jquery/dist/jquery.js"></script>
+    <?php wp_footer(); ?>
+    <!-- <script src="bower_components/jquery/dist/jquery.js"></script>
     <script src="bower_components/what-input/what-input.js"></script>
-    <script src="bower_components/foundation-sites/dist/foundation.js"></script>
-    <script src="js/app.js"></script>
+    <script src="bower_components/foundation-sites/dist/foundation.js"></script> -->
+    <!-- <script src="js/app.js"></script> -->
   </body>
 </html>
